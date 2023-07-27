c70d2a8d1048ac9c20ebb8820f6cfeafc882c636
SRYANG <sarang628@naver.com>
2023-07-27 17:51:53 +0800

    update

--- library/src/main/java/com/sryang/library/SelectPicture.kt
+++ library/src/main/java/com/sryang/library/SelectPicture.kt
@@ -6,12 +6,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -19,8 +21,16 @@ import androidx.compose.ui.unit.dp
 @Preview
 @Composable
 fun AddReview() {
+
+    //val testImage = [R.drawable.a, R.drawable.b]
+
+
     Column {
-        Row {
+        Row(
+            Modifier
+                .height(50.dp)
+                .padding(start = 8.dp, end = 8.dp), verticalAlignment = Alignment.CenterVertically
+        ) {
             Image(
                 painter = painterResource(id = android.R.drawable.ic_delete),
                 contentDescription = ""
@@ -32,15 +42,22 @@ fun AddReview() {
         }
 
         Image(
-            painter = painterResource(id = android.R.drawable.star_big_on),
+            painter = painterResource(id = R.drawable.a),
             contentDescription = "",
+            contentScale = ContentScale.Crop,
             modifier = Modifier
                 .fillMaxWidth()
                 .height(350.dp)
         )
 
-        Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
-            Text(modifier = Modifier.weight(1f), text = "최근항목")
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+                .padding(start = 8.dp, end = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(modifier = Modifier.weight(1f), text = "최근항목 >")
             Image(
                 painter = painterResource(id = android.R.drawable.ic_menu_agenda),
                 contentDescription = ""
@@ -52,15 +69,45 @@ fun AddReview() {
             )
         }
 
+        val testImage = arrayListOf<Int>(
+            R.drawable.a,
+            R.drawable.b,
+            R.drawable.c,
+            R.drawable.d,
+            R.drawable.e,
+            R.drawable.f,
+            R.drawable.g,
+            R.drawable.h,
+            R.drawable.i,
+            R.drawable.j,
+            R.drawable.k,
+            R.drawable.l,
+            R.drawable.m,
+            R.drawable.n,
+            R.drawable.o,
+            R.drawable.p,
+            R.drawable.q,
+            R.drawable.r,
+            R.drawable.s,
+            R.drawable.t,
+            R.drawable.u,
+            R.drawable.v,
+            R.drawable.w,
+            R.drawable.x,
+            R.drawable.y,
+            R.drawable.z
+        )
+
         LazyVerticalGrid(
             modifier = Modifier
                 .fillMaxWidth(),
-            columns = GridCells.Adaptive(minSize = 70.dp), content = {
-                items(1000) {
+            columns = GridCells.Adaptive(minSize = 80.dp), content = {
+                items(1000) { it ->
                     Image(
-                        modifier = Modifier.height(70.dp),
-                        painter = painterResource(id = android.R.drawable.star_on),
-                        contentDescription = ""
+                        modifier = Modifier.height(80.dp),
+                        painter = painterResource(id = testImage[it % testImage.size]),
+                        contentDescription = "",
+                        contentScale = ContentScale.Crop
                     )
                 }
             })
